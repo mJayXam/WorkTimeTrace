ecc2ab8cbd070fade55d5ae0fb88a51b53b0137f
Max <maja1006@stud.hs-kl.de>
2023-12-18 16:51:36 +0100

    Initial commit

---
+++ WorkTimeTrace/src/main/java/webapp/WorkTimeTrace/RestController/Rest.java
@@ -0,0 +1,14 @@
+package webapp.WorkTimeTrace.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
---
+++ WorkTimeTrace/src/test/java/webapp/WorkTimeTrace/WorkTimeTraceApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.WorkTimeTrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTimeTraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
