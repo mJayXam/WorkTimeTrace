e74ed8928a0ef20a00bdfebe2ffde0d33109bece
Nicolas Germann <86136825+NicolasGermann@users.noreply.github.com>
2024-01-13 13:54:11 +0100

    Revert "Basic database jpa hourmanagement"

--- timemanagement/src/main/java/com/worktimetrace/timemanagement/Database/StundenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.worktimetrace.timemanagement.Database;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-
-public interface StundenRepo extends CrudRepository<Stunden, Long> {
-    List<Stunden> findByNutzerid(Long nutzerid);
-}
