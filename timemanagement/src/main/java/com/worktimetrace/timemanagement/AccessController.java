b7e56c3550d5ba610859835f727ff1e791b28806
nige1001 <nige1001@stud.hs-kl.de>
2024-01-13 13:38:49 +0100

    RestSchnittstellen

--- timemanagement/src/main/java/com/worktimetrace/timemanagement/AccessController.java
+++ timemanagement/src/main/java/com/worktimetrace/timemanagement/AccessController.java
@@ -6,11 +6,17 @@ import com.worktimetrace.timemanagement.Database.Stunden;
 import com.worktimetrace.timemanagement.Database.StundenRepo;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -21,20 +27,38 @@ public class AccessController {
     StundenRepo rep;
     
     @GetMapping("/all")
-    public ArrayList<Stunden> findAll() {
+    public ResponseEntity<ArrayList<Stunden>> findAll() {
         ArrayList<Stunden> ret = new ArrayList<Stunden>();
         rep.findAll().forEach(ret::add);
-        return ret;
+        return ResponseEntity.ok(ret);
     }
 
     @GetMapping("/byID/{param}")
-    public Stunden getMethodName(@PathVariable Long param) {
+    public ResponseEntity<Stunden> findById(@PathVariable Long param) {
         Optional<Stunden> request = rep.findById(param);
         if (request.isPresent())
-            return request.get();
+            return ResponseEntity.ok(request.get());
         else
             return null;
     }
     
-
+    @PostMapping("/insertOne")
+    public ResponseEntity<String> insertOne(@RequestBody Stunden entity) {
+        rep.save(entity);
+        return ResponseEntity.ok("Speichern Erfolgreich");
+    }
+    
+    @PostMapping("/insertMany")
+    public ResponseEntity<String> insertMany(@RequestBody List<Stunden> entity) {
+        rep.saveAll(entity);
+        return ResponseEntity.ok("Spechern Erfolgreich");
+    }
+    
+    @GetMapping("/byNID/{param}")
+    public ResponseEntity<ArrayList<Stunden>> findByNID(@PathVariable Long param) {
+        ArrayList<Stunden> ret = new ArrayList<>();
+        rep.findByNutzerid(param).forEach(ret::add);
+        return ResponseEntity.ok(ret);
+    }
+    
 }
