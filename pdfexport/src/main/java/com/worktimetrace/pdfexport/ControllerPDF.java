cf8b58d63ce61bdfd6c66d6c1f3291e85b88bd84
nige1001 <nige1001@stud.hs-kl.de>
2024-01-16 15:58:56 +0100

    modified:   timemanagement/src/main/resources/application.properties

--- pdfexport/src/main/java/com/worktimetrace/pdfexport/ControllerPDF.java
+++ pdfexport/src/main/java/com/worktimetrace/pdfexport/ControllerPDF.java
@@ -32,12 +32,23 @@ public class ControllerPDF {
     @GetMapping("bill/{user}/{rate}")
     public void getMethodName(@PathVariable Long user, @PathVariable float rate, HttpServletResponse response) throws DocumentException, IOException {
         ArrayList<Stunden> hourList = getHourList(user);
+        byte[] pdfBytes = formatPDFDocument(hourList, rate);
+
+        response.setContentType("application/pdf");
+        response.setContentLength(pdfBytes.length);
+        response.setHeader("Content-Disposition", "attachment; filename=example.pdf");
+
+        response.getOutputStream().write(pdfBytes);
+        response.getOutputStream().flush();
+    }
+
+    private byte[] formatPDFDocument(ArrayList<Stunden> hourList, float rate) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfWriter writer = new PdfWriter(baos);
         PdfDocument pdf = new PdfDocument(writer);
         PdfPage page = pdf.addNewPage();
-
         PdfCanvas canvas = new PdfCanvas(page);
+
         float borderWidth = 2f;
         Color borderColor = ColorConstants.BLACK;
 
@@ -47,38 +58,81 @@ public class ControllerPDF {
         canvas.rectangle(borderWidth / 2 + 10, borderWidth / 2 + 10, page.getPageSize().getWidth() - borderWidth - 20, page.getPageSize().getHeight() - borderWidth - 20);
         canvas.stroke();
 
-        // Add your content to the page here
-        float yPosition = page.getPageSize().getHeight() - 50;
+        var yPos = addHourCounter(hourList, canvas, borderColor, 
+            page.getPageSize().getHeight() - 300, 
+            100);
+
+        yPos = addQuota(hourList, canvas, borderColor, rate,
+            yPos, 
+            100);
+        
+        pdf.close();
+        byte[] pdfBytes = baos.toByteArray();
+        return pdfBytes;
+    }
+
+    private float addQuota(ArrayList<Stunden> hourList, PdfCanvas canvas, Color borderColor, float rate, float y, float x) throws IOException {
+        var yPos = y;
+        var xPos = x;
+
+        String rateS = "Errechnete Gesamtkosten nach Satz: " + rate + "€ / std: ";
+        String quotaS = "" + hourList.stream().mapToDouble(Stunden::getStundenanzahl).sum() + " * " + rate + " € / std" + " = " +
+            hourList.stream().mapToDouble(Stunden::getStundenanzahl).sum() * rate;
+
+        canvas.beginText()
+            .setFontAndSize(com.itextpdf.kernel.font.PdfFontFactory.createFont(), 12) // Set your font and size
+            .setColor(ColorConstants.BLACK, true)
+            .moveText(xPos, yPos) // Adjust these coordinates as needed
+            .showText(rateS)
+            .endText();
+
+        yPos -= 15;
+
+        canvas.beginText()
+            .setFontAndSize(com.itextpdf.kernel.font.PdfFontFactory.createFont(), 12) // Set your font and size
+            .setColor(ColorConstants.BLACK, true)
+            .moveText(xPos, yPos) // Adjust these coordinates as needed
+            .showText(quotaS)
+            .endText();
+        
+        return yPos;
+    }
+
+    private float addHourCounter(ArrayList<Stunden> hourList, PdfCanvas canvas, Color borderColor, float y,
+            float x) throws IOException {
+        var yPosHour = y;
+        var xPosHour = x;
         canvas.beginText()
                 .setFontAndSize(com.itextpdf.kernel.font.PdfFontFactory.createFont(), 12) // Set your font and size
                 .setColor(ColorConstants.BLACK, true)
-                .moveText(50, yPosition) // Adjust these coordinates as needed
+                .moveText(xPosHour, yPosHour) // Adjust these coordinates as needed
                 .showText("Stundenanzahl:")
                 .endText();
 
-        yPosition -= 15;
+        yPosHour -= 15;
         for (var e : hourList) {
             canvas.beginText()
                     .setFontAndSize(com.itextpdf.kernel.font.PdfFontFactory.createFont(), 12) // Set your font and size
                     .setColor(ColorConstants.BLACK, true)
-                    .moveText(50, yPosition)
-                    .showText(e.getStundenanzahl().toString())
+                    .moveText(xPosHour + 20, yPosHour)
+                    .showText(e.getStundenanzahl().toString() + " +")
                     .endText();
-            yPosition -= 15;
+            yPosHour -= 15;
         }
 
-        canvas.endText();
+        canvas.setStrokeColor(borderColor);
+        canvas.setLineWidth(1f);
+        canvas.moveTo(xPosHour, yPosHour + 12);
+        canvas.lineTo(xPosHour + 100, yPosHour + 12);   
+        canvas.stroke();
 
-        pdf.close();
-
-        byte[] pdfBytes = baos.toByteArray();
-
-        response.setContentType("application/pdf");
-        response.setContentLength(pdfBytes.length);
-        response.setHeader("Content-Disposition", "attachment; filename=example.pdf");
-
-        response.getOutputStream().write(pdfBytes);
-        response.getOutputStream().flush();
+        canvas.beginText()
+                    .setFontAndSize(com.itextpdf.kernel.font.PdfFontFactory.createFont(), 12) // Set your font and size
+                    .setColor(ColorConstants.BLACK, true)
+                    .moveText(xPosHour + 20, yPosHour)
+                    .showText(hourList.stream().mapToDouble(Stunden::getStundenanzahl).sum() + "")
+                    .endText();
+        return yPosHour-15;
     }
 
     private ArrayList<Stunden> getHourList(Long user) {
