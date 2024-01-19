15a278cb6075ab7115af87e776bc9687723bf587
Max <maja1006@stud.hs-kl.de>
2024-01-19 13:36:04 +0100

    index.html update, bootstrap template verwendet und mit alter index.html kombiniert

--- frontend/src/main/java/com/worktimetrace/frontend/Controller/UIController.java
+++ frontend/src/main/java/com/worktimetrace/frontend/Controller/UIController.java
@@ -1,15 +1,25 @@
 package com.worktimetrace.frontend.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class UIController {
     
     @GetMapping("/")
-    public String getWebsite() {
+    public String getWebsite(Model model) {
+        model.addAttribute("kalender", true);
         return "index";
     }
+
+    @GetMapping("/Monatsuebersicht")
+    public String getMonatsuebersicht(Model model) {
+        model.addAttribute("monatsuebersicht", true);
+        return "index";
+    }
+    
     
 }
