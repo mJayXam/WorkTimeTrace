dcd9113c44ac39bbb272680a9d892bba1319cf2d
Joshua Ginkel <joshua.ginkel@iese.fraunhofer.de>
2024-01-24 15:13:15 +0100

    Delete UserManagement

--- usermanagement/src/main/java/com/worktimetrace/usermanagement/DTO/LoginDTO.java
+++
@@ -1,8 +0,0 @@
-package com.worktimetrace.usermanagement.DTO;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginDTO(
-        @NotBlank(message = "Nutzername darf nicht leer sein") String username,
-        @NotBlank(message = "Passwort darf nicht leer sein") String password) {
-}
--- usermanagement/src/main/java/com/worktimetrace/usermanagement/DTO/LoginResponseDTO.java
+++
@@ -1,3 +0,0 @@
-package com.worktimetrace.usermanagement.DTO;
-
-public record LoginResponseDTO(String username, String token) {}
