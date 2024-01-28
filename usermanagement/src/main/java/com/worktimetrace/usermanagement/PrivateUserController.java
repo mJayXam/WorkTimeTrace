f7651924856656b2dfd44a29e31ca63182ed7ba4
Joshua Ginkel <joshua.ginkel@iese.fraunhofer.de>
2024-01-28 09:52:43 +0100

    Add ID to UserInfo

--- usermanagement/src/main/java/com/worktimetrace/usermanagement/PrivateUserController.java
+++ usermanagement/src/main/java/com/worktimetrace/usermanagement/PrivateUserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.worktimetrace.usermanagement.DTO.UserInfoDTO;
